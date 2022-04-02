a720c0da2649c38423c013957776f37e2b3cd816
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-04-01 21:38:35 -0300

    botão de realizar pedido implementado  Changes to be committed: 	modified:   css/estilo.css 	modified:   index.html 	modified:   javascript/javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,4 +5,11 @@ function seleciona(clicado, tipo){
     }
     
     clicado.classList.add("selecionado");
+//esse if checa para ver se as tres opções foram selecionadas
+    if((document.querySelector(".prato .selecionado")!==null)&&(document.querySelector(".bebida .selecionado")!==null)&&(document.querySelector(".sobremesa .selecionado")!==null)){
+        const botao = document.querySelector(".baixo-fixo button");
+        botao.innerText=("Realizar pedido")
+        botao.classList.add("fechado")
+        
+    }
 }
