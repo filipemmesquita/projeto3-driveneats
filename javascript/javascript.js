7572e1f03348755117cd9677d3a932c80fc8b048
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-04-01 22:45:59 -0300

    implementei o tique verdes  Changes to be committed: 	modified:   css/estilo.css 	modified:   index.html 	modified:   javascript/javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,17 @@
 function seleciona(clicado, tipo){
-    let selecionado = (document.querySelector(tipo+" .selecionado"));
-    if(selecionado !== null){
-        selecionado.classList.remove("selecionado");
+    let selecaoAnterior = (document.querySelector(tipo+" .selecionado"));
+    let tiqueAnterior = (document.querySelector(tipo+" .selecionado .tique"));
+    if(selecaoAnterior !== null){
+        selecaoAnterior.classList.remove("selecionado");
+        selecaoAnterior.style.cssText ="Border-color:#ffffff";
+        tiqueAnterior.style.cssText ="visibility:hidden;"
     }
     
     clicado.classList.add("selecionado");
+    clicado.style.cssText ="Border-color:#32B72F";
+    tiqueClicado = (clicado.querySelector(".tique"));
+    tiqueClicado.style.cssText ="visibility:visible;"
+
 //esse if checa para ver se as tres opções foram selecionadas
     if((document.querySelector(".prato .selecionado")!==null)&&(document.querySelector(".bebida .selecionado")!==null)&&(document.querySelector(".sobremesa .selecionado")!==null)){
         const botao = document.querySelector(".baixo-fixo button");
