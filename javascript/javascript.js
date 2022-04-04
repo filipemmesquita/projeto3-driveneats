a0394b63dbee54a61b9c7360b6e2865da6288671
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-04-04 10:53:42 -0300

    implementado pedir nome e endereço  Changes to be committed: 	modified:   index.html 	modified:   javascript/javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -17,28 +17,29 @@ function seleciona(clicado, tipo){
         const botao = document.querySelector(".baixo-fixo button");
         botao.innerText=("Realizar pedido")
         botao.classList.add("fechado")
-        let nome = prompt("Por favor insira seu nome!");
-        let endereco = prompt("Por favor insira seu endereço!");
-        let prato= document.querySelector(".prato .selecionado h2").innerText;
-        let preçoprato = parseFloat(document.querySelector(".prato .selecionado span").innerText);
-        let bebida= document.querySelector(".bebida .selecionado h2").innerText;
-        let preçobebida = parseFloat(document.querySelector(".bebida .selecionado span").innerText);
-        let sobremesa= document.querySelector(".sobremesa .selecionado h2").innerText;
-        let preçosobremesa = parseFloat(document.querySelector(".sobremesa .selecionado span").innerText);
-        let preçototal= preçobebida+preçoprato+preçosobremesa;
-        let mensagem = encodeURIComponent(`Olá, gostaria de fazer o pedido:
-        - Prato: ${prato}
-        - Bebida: ${bebida}
-        - Sobremesa: ${sobremesa}
-        Total: R$ ${preçototal.toFixed(2)}
 
-        Nome: ${nome}
-        Endereço: ${endereco}`)
-        document.querySelector(".baixo-fixo a").href ="https://wa.me/5521981907552?text="+mensagem;
-        //href="https://wa.me/5521981907552?text=testes"
-        
     }
 }
-function envia(){
+function envio(){
+    let nome = prompt("Por favor insira seu nome!");
+    let endereco = prompt("Por favor insira seu endereço!");
+    let prato= document.querySelector(".prato .selecionado h2").innerText;
+    let preçoprato = parseFloat(document.querySelector(".prato .selecionado span").innerText);
+    let bebida= document.querySelector(".bebida .selecionado h2").innerText;
+    let preçobebida = parseFloat(document.querySelector(".bebida .selecionado span").innerText);
+    let sobremesa= document.querySelector(".sobremesa .selecionado h2").innerText;
+    let preçosobremesa = parseFloat(document.querySelector(".sobremesa .selecionado span").innerText);
+    let preçototal= preçobebida+preçoprato+preçosobremesa;
+    let mensagem = encodeURIComponent(`Olá, gostaria de fazer o pedido:
+    - Prato: ${prato}
+    - Bebida: ${bebida}
+    - Sobremesa: ${sobremesa}
+    Total: R$ ${preçototal.toFixed(2)}
+
+    Nome: ${nome}
+    Endereço: ${endereco}`)
+    document.querySelector(".baixo-fixo a").href ="https://wa.me/5521981907552?text="+mensagem;
+    //href="https://wa.me/5521981907552?text=testes"
+    
     
 }
